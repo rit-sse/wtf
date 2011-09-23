c95d89d99fd2def3afd1381a9995c74844202bd1
Cody Krieger <cody@codykrieger.com>
2011-09-23 13:22:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wtf::Application.config.secret_token = '8cac6eacb568bd5a9263cfebc74b8a47553c47538905cf936d329422686fb13f281dc755de4a1885e87eb69245d90bf0e2ac6409c0e8a14c022829659c899d85'
