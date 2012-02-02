da55d19fab2ec1d313620bf51ce5a8c40f1ec267
Cody Krieger <cody@codykrieger.com>
2012-02-02 03:08:57 -0500

    Active links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,15 @@ module ApplicationHelper
     end
   end
 
+  def active_link_to(title, url)
+    if url == '/'
+      css = "active" if request.path == '/'
+    elsif request.path.start_with? url
+      css = "active"
+    end
+    link_to title, url, class: css
+  end
+
   def markdown(text)
     options = {
       autolink: true,
