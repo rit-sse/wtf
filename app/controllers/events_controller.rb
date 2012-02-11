4fac335216799a6291310cedfe2f1389ed37315c
Dorrene Brown <dorreneb@gmail.com>
2012-02-11 14:27:10 -0500

    Can filter by committee, even though it doesn't look nice at all yet.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,6 +24,11 @@ class EventsController < AdminController
       @events = Event.where(:start_date => params[:start_date].to_date..params[:start_date].to_date.next_month)
     end
 
+	if params[:filter] != nil
+	  committee = Committee.where(:name => params[:filter]).first.id
+      @events = @events.where(:committee_id => committee)
+	end
+
     respond_to do |format|
       format.html do # index.html.erb
         if params[:view] == "calendar"
