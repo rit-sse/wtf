65e71c55b8a67a5d4e6d1b060db71c849d33e6d4
Brian Wyant <wyantb@gmail.com>
2012-04-03 13:55:36 -0400

    Use priv credentials, if they exist.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,25 +1,28 @@
 
-if Rails.env.test? or 
+if FileTest.exists?(Rails.root.join('config/s3_credentials.yml'))
+
+  puts "Using real credentials"
+  Fog.credentials_path = Rails.root.join('config/s3_credentials.yml')
+
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {:provider => 'AWS'}
+    config.fog_directory = "images" # required
+  end
+elsif Rails.env.test? or 
   Rails.env.cucumber? or 
   Rails.env.development?
 
-  puts "Mocking S3 Image Storage"
-  Fog.mock!
-end
+  puts "Using filesystem for uploads"
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
 
-Fog.credentials_path = Rails.root.join('config/fog_credentials.yml')
-
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_credentials = {:provider => 'AWS'}
-  config.fog_directory = "images" # required
-end
-
-if Rails.env.test? or 
-  Rails.env.cucumber? or 
-  Rails.env.development?
-
-  puts "Setting up fake S3 connection"
-  connection = Fog::Storage.new(:provider => 'AWS')
-  connection.directories.create(:key => 'images')
+  # puts "Mocking S3 Image Storage"
+  # Fog.mock!
+  # puts "Setting up fake S3 connection"
+  # connection = Fog::Storage.new(:provider => 'AWS')
+  # connection.directories.create(:key => 'images')
+else
+  puts "Error!  No method to store files configured."
 end
