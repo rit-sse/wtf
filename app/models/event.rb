c6ead3375ca614c3957e2dbc2fa1c40218d163d6
Chris Bentivenga <chris.bentivenga@me.com>
2012-09-25 16:03:33 -0400

    Added in iCal (ics) support from the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
+require 'ri_cal'
+
 class Event < ActiveRecord::Base
   validates_presence_of :name, :short_name
   validates_uniqueness_of :name
@@ -14,6 +16,20 @@ class Event < ActiveRecord::Base
   def generate_short_name
     if self.short_name.to_s == ""
       self.short_name = self.name
+    end
+  end
+
+  def Event.to_ical
+    RiCal.Calendar do |cal|
+      Event.all.each do |model_event|
+        cal.event do |ical_event|
+          ical_event.summary     = model_event.short_name
+          ical_event.description = model_event.description
+          ical_event.dtstart     = model_event.start_date
+          ical_event.dtend       = model_event.end_date
+          ical_event.location    = model_event.location
+        end
+      end
     end
   end
 end
