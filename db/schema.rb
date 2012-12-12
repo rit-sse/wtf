673dabc5d683760f63ae1e65fa27b3d3e18d2650
seancongden <sean.congden@gmail.com>
2012-12-12 11:24:36 -0500

    Remove content from pages (now in blocks)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121211203235) do
+ActiveRecord::Schema.define(:version => 20121212161945) do
 
   create_table "blocks", :force => true do |t|
     t.integer  "page_id",          :null => false
@@ -55,11 +55,9 @@ ActiveRecord::Schema.define(:version => 20121211203235) do
     t.string   "title"
     t.string   "slug"
     t.datetime "published_at"
-    t.text     "content"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
     t.string   "ancestry"
-    t.string   "content_type"
     t.string   "layout",       :null => false
   end
 
