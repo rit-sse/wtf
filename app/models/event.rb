31ebcee5397991738eebaad0e8a884d9470fff45
Chris Bentivenga <chris@chrisbenti.com>
2013-03-12 15:23:28 -0400

    Adds server side validation (with tests)

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,14 +2,13 @@ require 'ri_cal'
 require 'csv'
 
 class Event < ActiveRecord::Base
-  validates_presence_of :name, :short_name
+  validates_presence_of :name, :short_name, :location, :committee
+  validates :short_name, :length => { minimum: 1,  maximum: 25 }
 
   has_many :event_prices
   belongs_to :committee
 
   mount_uploader :image, ImageUploader
-
-  before_validation :generate_short_name
 
   ##
   # Returns the week of the event in a hash of the format:
