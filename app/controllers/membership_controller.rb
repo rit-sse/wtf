32981ddc1bbba902334245dc1d35ee65f266c019
Wesley Wigham <wes@ham.io>
2013-11-12 13:24:12 -0500

    Changed some styles, made all buttons bootstrap, checks semesters

--- app/controllers/membership_controller.rb
+++ app/controllers/membership_controller.rb
@@ -1,8 +1,30 @@
 class MembershipController < ApplicationController
   def index
-    puts "whaaaat"
     if params[:q]
-      @results = Membership.where(:dce => params[:q])
+      now = Date.today
+      # Membership 'blocks'
+      # memberships are good to 2 weeks into the next semester
+      # August 20 - (January 27 + 2 weeks)
+      # January 27 - (August 20 + 2 weeks)
+      jan27 = Date.new(
+        now.year,
+        1,
+        27,
+      )
+      aug20 = Date.new(
+        now.year,
+        8,
+        20,
+      )
+      if (now >= jan27 and now <= jan27+2.weeks) 
+        @results = Membership.where(dce: params[:q], date: (aug20-1.year)..now)
+      elsif (now >= aug20 and now <= aug20+2.weeks)
+        @results = Membership.where(dce: params[:q], date: (jan27-1.year)..now)
+      elsif (now>=jan27+2.weeks and now<=aug20)
+        @results = Membership.where(dce: params[:q], date: jan27..now)
+      else
+        @results = Membership.where(dce: params[:q], date: aug20..now)
+      end
     end
   end
   
@@ -20,11 +42,13 @@ class MembershipController < ApplicationController
   # POST /committees/new
   # POST /committees/new.json
   def create
-    @membership = Membership.new(params[:membership])
-    @membership.save
+    if signed_in?
+      @membership = Membership.new(params[:membership])
+      @membership.save
+    end
     
     respond_to do |format|
-      format.html # new.html.erb
+      format.html # create.html.erb
       format.json { render json: @membership }
     end
   end
