54a4956ec31e3a50ebbbac3e65f7d88926045301
7imbrook <timbrook480@gmail.com>
2013-12-10 19:36:11 -0500

    Updated SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Wtf::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
+  config.force_ssl = true
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
