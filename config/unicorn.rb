c2b99e941257044bd7e9caeebfc926b8aec4958c
7imbrook <timbrook480@gmail.com>
2013-12-10 21:19:37 -0500

    Updated Unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -39,7 +39,25 @@ after_fork do |server, worker|
   # Unicorn master loads the app then forks off workers - because of the way
   # Unix forking works, we need to make sure we aren't using any of the parent's
   # sockets, e.g. db connection
-
   ActiveRecord::Base.establish_connection
+
+  begin
+    uid, gid = Process.euid, Process.egid
+    user, group = 'deployservice', 'website'
+    target_uid = Etc.getpwnam(user).uid
+    target_gid = Etc.getgrnam(group).gid
+    worker.tmp.chown(target_uid, target_gid)
+    if uid != target_uid || gid != target_gid
+      Process.initgroups(user, target_gid)
+      Process::GID.change_privilege(target_gid)
+      Process::UID.change_privilege(target_uid)
+    end
+  rescue => e
+    if RAILS_ENV == 'development'
+      STDERR.puts "couldn't change user, oh well"
+    else
+      raise e
+    end
+  end
 
 end
