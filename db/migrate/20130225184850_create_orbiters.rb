8257ab032c3c6020d4b6b134a7caadab2f614dc2
7imbrook <timbrook480@gmail.com>
2013-12-10 19:49:38 -0500

    Fixed Database

--- db/migrate/20130225184850_create_orbiters.rb
+++ db/migrate/20130225184850_create_orbiters.rb
@@ -1,7 +1,7 @@
 class CreateOrbiters < ActiveRecord::Migration
   def change
     create_table :orbiters do |t|
-      t.string :content
+      t.text :content
       t.datetime :updated_at
       t.datetime :created_at
 
