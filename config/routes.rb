93ff19c1fe391ed55d89b80d171dfccc2625f883
kristenmills <kristen@kristen-mills.com>
2014-02-21 12:56:13 -0500

    Removing routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,34 +3,33 @@ Wtf::Application.routes.draw do
 
   scope "/admin" do
     resources :events
-    resources :committees
-    resources :uploads
+    # resources :committees
+    # resources :uploads
 
     match '/events' => 'events#index', as: "admin_events"
-    
-    get '/orbiter/add'
-    get '/orbiter/destroy'
-    match '/orbiter/edit' => 'orbiter#edit', :via => [:post]
+    # get '/orbiter/add'
+    # get '/orbiter/destroy'
+    # match '/orbiter/edit' => 'orbiter#edit', :via => [:post]
 
     match '/diagnostics(/:action)', controller: 'diagnostics' if Rails.env.development?
 
-    root to: 'admin#index', as: "admin"
+    # root to: 'admin#index', as: "admin"
   end
 
-  namespace "admin" do
-    resources :pages
-    resources :blocks
-  end
+  # namespace "admin" do
+     resources :pages
+  #   resources :blocks
+  # end
 
   match '/login', to: 'auth#index'
   match '/logout', to: 'auth#logout'
   post '/auth(/:action)', controller: 'auth'
 
-  match '/events' => 'events#public_index', as: "events_public_events"
-  match '/events/current', to: 'events#current'
-  match '/events/gtv', to: 'events#gtv'
-  match '/events/ftv', to: 'events#ftv'
-  match '/events(/:id)' => 'events#public_show', as: "events_public_show"
+  # match '/events' => 'events#public_index', as: "events_public_events"
+  # match '/events/current', to: 'events#current'
+  # match '/events/gtv', to: 'events#gtv'
+  # match '/events/ftv', to: 'events#ftv'
+  # match '/events(/:id)' => 'events#public_show', as: "events_public_show"
 
   # static routes and redirects
   # ...
@@ -39,7 +38,7 @@ Wtf::Application.routes.draw do
   root :to => 'root#index'
 
   # for dynamic pages
-  match '/*path', to: 'pages#dynamic_page'
+  # match '/*path', to: 'pages#dynamic_page'
 
   # See how all your routes lay out with "rake routes"
 
