ea4716007759af0e0d964ef463d3c922695050d6
kristenmills <kristen@kristen-mills.com>
2014-03-26 13:56:23 -0400

    updated secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,6 +7,6 @@
 secret_file_path = "#{Rails.root}/session_key"
 secret_key = "Ouppvx4UKRIJ7zHCDuFEYh7IOwaJ3dIClmROlIzj5Y5RkSVeN2CIZMOar6FxwYL"
 if File.exist? secret_file_path
-  secret_key = File.read secret_file_path
+  secret_key = File.read(secret_file_path).chomp
 end
 Wtf::Application.config.secret_token = secret_key
