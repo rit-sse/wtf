4ab4e8b8780f83b7dba67bb7d72b311a192ec039
kristenmills <kristen@kristen-mills.com>
2014-03-26 13:53:55 -0400

    Updatedc session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Wtf::Application.config.session_store :cookie_store, key: '_sse_session', domain: '.sofse.org'
-
+Wtf::Application.config.session_store :cookie_store, key: '_sse_session'
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
